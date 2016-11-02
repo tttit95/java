349a025f87f45ff5ff782d78c148946eb9411ac4
tttit95 <ttt.it95@gmail.com>
2016-11-02 07:41:37 +0700

    kiemtra211

--- jsp_servlet/TranThanhTaiv4/src/DungChung/CDungChung.java
+++
@@ -1,7 +0,0 @@
-package DungChung;
-
-public class CDungChung {
-	public static String url = "jdbc:sqlserver://U7FJWMER4GFLISX\\SQL2012:1433;databaseName=TranThanhTai";
-	public static String un="sa";
-	public static String pass="123456";
-}
--- jsp_servlet/TranThanhTaiv4/src/Model/DAO/DonViDAO.java
+++
@@ -1,5 +0,0 @@
-package Model.DAO;
-
-public class DonViDAO {
-
-}
